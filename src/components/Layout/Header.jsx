3040dc0a569d1aab2e767c716059db1dce88b0f0
Jhon <jhonalvertmarbebe0@gmail.com>
2026-03-06 20:38:05 +0800

    update nav

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [openNav, setOpenNav] = useState(false);
   return (
     <header className="h-15">
-      <div className="fixed flex justify-between items-center w-full left-0 right-0 top-0  px-5 py-3 lg:max-w-7xl lg:mx-auto lg:justify-normal">
+      <div className="fixed flex justify-between items-center w-full left-0 right-0 top-0  px-5 py-3 lg:max-w-7xl lg:mx-auto ">
         <span>
           <a href="">Logo</a>
         </span>
--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -6,26 +6,26 @@ const Nav = ({ toggle }) => {
     <nav
       className={`flex flex-col left-0 right-0 p-5 h-screen rounded-lg absolute top-10
   transition-all duration-300 ease-in-out
-  ${toggle ? "opacity-100 translate-y-2 pointer-events-auto backdrop-blur-xs" : "opacity-0 translate-y-0 pointer-events-none"} flex flex-col items-center lg:static lg:h-auto lg:p-0 lg:opacity-100 lg:backdrop-blur-none lg:flex-row lg:flex-1 lg:justify-center lg:pointer-events-auto `}
+  ${toggle ? "opacity-100 translate-y-2 pointer-events-auto backdrop-blur-xs" : "opacity-0 translate-y-0 pointer-events-none"} flex flex-col items-center lg:static lg:h-auto lg:p-0 lg:opacity-100 lg:backdrop-blur-none lg:flex-row lg:pointer-events-auto lg:px-3 lg:py-2 lg:bg-gray-200 lg:gap-4 lg:rounded-4xl  `}
     >
-      <ul className="font-medium flex flex-col gap-4 text-center bg-neutral-200 p-5 rounded-lg w-full md:w-140 lg:flex-row lg:bg-transparent lg:p-0 lg:mx-auto lg:sticky ">
-        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0 ">
+      <ul className="text-[#7AAACE] font-medium flex flex-col gap-4 text-center bg-neutral-200 p-5 rounded-lg w-full md:w-140 lg:flex-row lg:bg-transparent lg:p-0  lg:sticky lg:w-auto lg:gap-8 lg:ml-3 ">
+        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0 lg:w-auto ">
           <a href="#home">Home</a>
         </li>
-        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0">
+        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0  lg:w-auto ">
           <a href="#about">Skill</a>
         </li>
-        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0">
+        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0  lg:w-auto">
           <a href="">Project</a>
         </li>
-        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0">
+        <li className="bg-neutral-100 w-full px-5 py-3 rounded-md lg:bg-transparent lg:p-0  lg:w-auto">
           <a href="">About</a>
         </li>
       </ul>
 
       <Button
         className={
-          "bg-blue-600 text-white hover:bg-blue-700 transition-all ease-in-out duration-150 cursor-pointer mt-3 lg:mt-0 "
+          "bg-[#7AAACE] text-[#EEE8E8] hover:bg-blue-700 transition-all ease-in-out duration-150 cursor-pointer mt-3 lg:mt-0 lg:rounded-4xl "
         }
         context={"contact Me"}
       />
